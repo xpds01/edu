eff1de62cbcc0bfb7aed52a82f9b5240be7af672
xpds <1337997135@qq.com>
2023-05-26 11:16:57 +0800

    课程模块的部分接口文档的发布和测试

---
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
@@ -0,0 +1,2 @@
+package com.xuecheng.content.api;public class CourseBaseInfoConntroller {
+}
---
+++ xuecheng-plus-content/xuecheng-plus-content-model/src/test/java/com/xuecheng/content/module/XuechengPlusContentModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuecheng.content.module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XuechengPlusContentModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
